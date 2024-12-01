f3899af991d3ded0402f7c491a74dcf6456c7861
chrisokoth <chrisokoth17@gmail.com>
2024-12-02 01:24:53 +0300

    queries with joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,10 @@
-SELECT Booking.booking_id, Booking.start_date, Booking.end_date, Booking.total_price, Booking.status, 
-       User.user_id, User.first_name, User.last_name, User.email
+SELECT User.user_id, User.first_name, User.last_name, User.email, 
+       Booking.booking_id, Booking.start_date, Booking.end_date, Booking.total_price, Booking.status
+FROM User
+LEFT JOIN Booking ON User.user_id = Booking.user_id
+UNION
+SELECT User.user_id, User.first_name, User.last_name, User.email, 
+       Booking.booking_id, Booking.start_date, Booking.end_date, Booking.total_price, Booking.status
 FROM Booking
-INNER JOIN User ON Booking.user_id = User.user_id;
+LEFT JOIN User ON User.user_id = Booking.user_id
+ORDER BY user_id;
