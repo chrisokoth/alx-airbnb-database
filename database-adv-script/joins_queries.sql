3f4c8ebab094abbc620fc7524c10269bd7b4e507
chrisokoth <chrisokoth17@gmail.com>
2024-12-02 01:31:43 +0300

    queries with joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,3 +2,10 @@ SELECT Booking.booking_id, Booking.start_date, Booking.end_date, Booking.total_p
        User.user_id, User.first_name, User.last_name, User.email
 FROM Booking
 INNER JOIN User ON Booking.user_id = User.user_id;
+
+SELECT Property.property_id, Property.name, Property.location, Property.pricepernight, 
+       Review.review_id, Review.rating, Review.comment
+FROM Property
+LEFT JOIN Review ON Property.property_id = Review.property_id
+ORDER BY Property.property_id;
+
