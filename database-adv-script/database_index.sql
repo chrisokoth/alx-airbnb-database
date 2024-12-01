f6fd79dcf978c3e4f70f683499a161e3b809bc04
chrisokoth <chrisokoth17@gmail.com>
2024-12-02 01:54:02 +0300

    implement indexes for optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -20,3 +20,8 @@ CREATE INDEX idx_property_id ON Property(property_id);
 CREATE INDEX idx_property_host_id ON Property(host_id);
 
 CREATE INDEX idx_property_location ON Property(location);
+
+
+-- Before adding indexes, measure query performance for the Booking table
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 123;
+
